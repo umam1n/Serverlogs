23d3d4252a9c77133d6b8a265a8474e2b69a2b50
umam1n <mushthafa.a.r@gmail.com>
2025-08-14 13:42:21 +0700

    Add pic not shown

--- Serverlogs/sites/admin.py
+++ Serverlogs/sites/admin.py
@@ -5,13 +5,19 @@ from .models import ServerLocation
 
 @admin.register(ServerLocation)
 class ServerLocationAdmin(admin.ModelAdmin):
-    # MODIFIED: Added 'display_pics' to the list
+    # This list controls the columns on the main list page
     list_display = ('name', 'address', 'display_pics', 'latitude', 'longitude')
+    
     search_fields = ('name', 'address')
-    filter_horizontal = ('pics',)
+    filter_horizontal = ('pics',) # Use a nicer widget for multiple users
+    
+    # These fields are still not editable by hand
     readonly_fields = ('latitude', 'longitude')
 
-    # ADDED: A custom method to display the PICs
+    # This function tells Django HOW to display the 'pics' field
     def display_pics(self, obj):
+        # Joins the full names of all assigned PICs with a comma
         return ", ".join([pic.get_full_name() for pic in obj.pics.all()])
+    
+    # This sets the column header name in the admin
     display_pics.short_description = 'PICs'
