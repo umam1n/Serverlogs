bd8ae6879a72cde37ad125c58cbb24b6b687ee7c
umam1n <mushthafa.a.r@gmail.com>
2025-08-14 13:37:57 +0700

    Add pic not shown

--- Serverlogs/sites/admin.py
+++ Serverlogs/sites/admin.py
@@ -1,8 +1,17 @@
-from django.contrib import admin
+# FILE: sites/admin.py
 
-# Register your models here.
 from django.contrib import admin
 from .models import ServerLocation
 
-# Register your models here.
-admin.site.register(ServerLocation)
+@admin.register(ServerLocation)
+class ServerLocationAdmin(admin.ModelAdmin):
+    # MODIFIED: Added 'display_pics' to the list
+    list_display = ('name', 'address', 'display_pics', 'latitude', 'longitude')
+    search_fields = ('name', 'address')
+    filter_horizontal = ('pics',)
+    readonly_fields = ('latitude', 'longitude')
+
+    # ADDED: A custom method to display the PICs
+    def display_pics(self, obj):
+        return ", ".join([pic.get_full_name() for pic in obj.pics.all()])
+    display_pics.short_description = 'PICs'
